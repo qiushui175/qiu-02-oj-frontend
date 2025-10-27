acde021127df7a85dc670e003683b401f027c41a
qiu <1753638325@qq.com>
2025-10-27 16:57:24 +0800

    优化权限校验和菜单显示逻辑

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,26 @@ import { useRouter } from 'vue-router'
 import BasicLayout from './layouts/BasicLayout.vue'
 import { useStore } from 'vuex'
 import checkAccess from './access/checkAccess'
+import { onMounted } from 'vue'
+
+// 预留全局初始化代码块
+const doInit = () => {
+  console.log('hello 欢迎来到我的项目')
+}
+onMounted(() => {
+  doInit()
+})
 
 // 全局权限管理
 const router = useRouter()
 const store = useStore()
 
 router.beforeEach((to, from, next) => {
-  if(checkAccess(store.state.user?.loginUser, to.meta.access)) next()
-  next("/noAuth")
+  if (checkAccess(store.state.user?.loginUser, to.meta.access)) {
+    next()
+    return
+  }
+  next('/noAuth')
 })
 </script>
 
